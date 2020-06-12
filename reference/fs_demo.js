1880a4585cca06da95ed4eb2a8dd2f40df787047
Ilgsson Braga <ilgssonbraga@gmail.com>
2020-06-12 17:54:24 -0300

    feat: fs module

--- reference/fs_demo.js
+++ reference/fs_demo.js
@@ -18,13 +18,38 @@ fs.writeFile(path.join(__dirname, 'test', 'hello.txt'), 'Hello, world!', err => 
         throw err;
     }
 
-    console.log('File written to...')
+    console.log('File written to...');
+
+    fs.appendFile(path.join(__dirname, 'test', 'hello.txt'), ' I love Node.js!', err => {
+        if(err){
+            throw err;
+        }
+    
+        console.log('File written to...');
+    });
 });
 
-fs.appendFile(path.join(__dirname, 'test', 'hello.txt'), ' I love Node.js!', err => {
-    if(err){
-        throw err;
-    }
+// fs.readFile(path.join(__dirname, '/test', 'hello.txt'), 'utf8', (err, data) => {
+//     if(err){
+//         throw err;
+//     }
 
-    console.log('File written to...')
-});
+//     console.log(data);
+// });
+
+// fs.rename(path.join(__dirname, '/test', 'hello.txt'), path.join(__dirname, '/test', 'hellochanged.txt'), err => {
+//     if(err){
+//         throw err;
+//     }
+
+//     console.log("Name changed...");
+// });
+
+// fs.unlink(path.join(__dirname, '/test', 'hellochanged.txt'), err => {
+//     if(err){
+//         throw err;
+//     }
+
+//     console.log("File deleted...");
+// });
+
